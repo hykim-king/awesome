9bbfad69955d474b6793a59a73c6462a38df4e32
Dashboard <dlwhd0614@naver.com>
2025-08-11 09:44:27 +0900

    수정 및 컨트롤러 테스트 진행

--- Awesome/src/main/java/com/pcwk/ehr/article/controller/ArticleController.java
+++ Awesome/src/main/java/com/pcwk/ehr/article/controller/ArticleController.java
@@ -20,49 +20,11 @@ import com.pcwk.ehr.article.service.ArticleService;
 @RequestMapping("/article")
 public class ArticleController {
 
+	Logger log = LogManager.getLogger(getClass());
 	
 	@Autowired
-	ArticleService service;
-	
-	//검색
-	@GetMapping("/list.do")
-	public String articleList(@RequestParam(value = "searchDiv", required = false) String searchDiv,
-							@RequestParam(value = "searchWord", required = false) String searchWord,
-							Model model) throws Exception {
-		
-		//검색 조건 설정
-		ArticleSearchDTO search = new ArticleSearchDTO();
-		search.setSearchDiv(searchDiv);
-		search.setSearchWord(searchWord);
-		
-		//기사 조회
-		List<ArticleDTO> list = service.doRetrieve(search);
-		
-		//결과 전달
-		model.addAttribute("list", list);
-		model.addAttribute("searchDiv", searchDiv);
-		model.addAttribute("searchWord", searchWord);
-						
-		return "article/list";
-	}
-	
-	@GetMapping("/category.do")
-	public String categoryByArticleList(@RequestParam("category") int category,
-										Model model) throws Exception {
-		ArticleSearchDTO search = new ArticleSearchDTO();
-		search.setCategory(category);
-		
-		List<ArticleDTO> list = service.doRetrieve(search);
-		
-		model.addAttribute("list", list);
-		model.addAttribute("category", category);
-		
-		
-		return "article/list";
-	}
-	
+	ArticleService service;	
 
-	Logger log = LogManager.getLogger(getClass());
 
 
 	// 페이징,검색 동시
