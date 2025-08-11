8405a8fa828573632b4f2e0991e40b09121a0d0c
ebh99 <bh990907@naver.com>
2025-08-11 17:43:07 +0900

    chatmessage, report test 중

---
+++ Awesome/src/main/java/com/pcwk/ehr/mapper/ChatMessageMapper.java
@@ -0,0 +1,27 @@
+package com.pcwk.ehr.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.pcwk.ehr.chatmessage.domain.ChatMessageDTO;
+import com.pcwk.ehr.cmn.WorkDiv;
+
+public interface ChatMessageMapper extends WorkDiv<ChatMessageDTO> {
+	
+	void deleteAll() throws SQLException;
+
+
+    // 채팅 특화 조회: 카테고리별 최신 N건
+    List<ChatMessageDTO> findRecentByCategory(
+    		@Param("category") int category,
+            @Param("limit") int limit);
+
+    // 무한스크롤: 특정 코드 이전 N건
+    List<ChatMessageDTO> findBeforeCode(
+    	    @Param("category") int category,
+    	    @Param("lastChatCode") int lastChatCode,
+    	    @Param("limit") int limit
+    	);
+}
