f9658b654359edaae5bb7f78977f59cc6838aab5
YangSeungHyunn <yangsh5972@gmail.com>
2025-08-18 18:02:57 +0900

    mypage jsp update

--- Awesome/src/main/java/com/pcwk/ehr/bookmark/controller/BookmarkController.java
+++ Awesome/src/main/java/com/pcwk/ehr/bookmark/controller/BookmarkController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
@@ -33,6 +34,11 @@ public class BookmarkController {
 		log.debug("│ BookmarkController()                  │");
 		log.debug("└───────────────────────────────────────┘");	
 		
+	}
+	
+	@GetMapping(value = "/mypage.do")
+	public String main() {
+		return "mypage/mypage";
 	}
 	
 	//로그인된 유저에 한 해 북마크 토글 기능
@@ -70,7 +76,7 @@ public class BookmarkController {
 		
 		return jsonString;
 	}
-	
+
 	
 	//기사 페이지에서 각 기사마다 북마크 여부 조회
 	@GetMapping(value = "/doSelectOne.do")
@@ -98,7 +104,12 @@ public class BookmarkController {
 	//같은 페이지를 그대로 렌더 + 모달 플래그 내려주기(로그인 팝업)
 	//마이페이지에서 내가 북마크한 기사 목록
 	@GetMapping(value = "/doRetriveMy.do")
-	public String doRetriveMy(BookmarkDTO param, Model model, HttpSession session) {
+	public String doRetriveMy( 
+			@RequestParam(defaultValue = "1") int pageNo,
+	        @RequestParam(defaultValue = "5") int pageSize,
+	        BookmarkDTO param, 
+	        Model model, 
+	        HttpSession session) {
 		String viewName = "/mypage/mypage";
 		log.debug("┌───────────────────────────────────────┐");
 		log.debug("│ doRetriveMy()                         │");
@@ -117,22 +128,24 @@ public class BookmarkController {
 		
 		// 2) 로그인된 경우만 조회
 		param.setUserId(userId); //세션에서 주입
+		param.setPageNo(pageNo);
+		param.setPageSize(pageSize);
 		List<BookmarkDTO> list = bookmarkService.doRetriveMy(param);
 		model.addAttribute("list", list);
 		log.debug("북마크 목록 건수: {}", (list != null ? list.size() : 0));
 		
-		if(list != null) {
-			model.addAttribute("bookmark", true);
-		}
+	    // 전체 건수 (서비스에서 별도로 조회)
+	    int totalCnt = bookmarkService.getCount(param);
+	    model.addAttribute("totalCnt", totalCnt);
+	    model.addAttribute("pageNo", pageNo);
+	    model.addAttribute("pageSize", pageSize);
 		
-		//북마크가 없는 경우 안내 문구 추가ㄴ
-		if(list == null || list.isEmpty()) {
-			model.addAttribute("noBookmarkMsg", "북마크한 기사가 없습니다.<br>핫이슈 '오늘의 토픽'을 살펴보세요!");
-			model.addAttribute("bookmark", list != null);
-		}
-		
-		 return viewName;
+
+	    if(list == null || list.isEmpty()) {
+	        model.addAttribute("noBookmarkMsg", "북마크한 기사가 없습니다.<br>핫이슈 '오늘의 토픽'을 살펴보세요!");
+	    }
+
+	    return viewName;
+	
 	}
-	
-	
 }
