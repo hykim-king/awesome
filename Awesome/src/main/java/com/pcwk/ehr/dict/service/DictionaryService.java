679e0e43eb3301c707aaa075e9e4fae0de207296
min <kamgyong@gmail.com>
2025-08-20 00:18:00 +0900

    딕셔너리 값 가져오기

--- Awesome/src/main/java/com/pcwk/ehr/dict/service/DictionaryService.java
+++ Awesome/src/main/java/com/pcwk/ehr/dict/service/DictionaryService.java
@@ -14,32 +14,36 @@ import com.pcwk.ehr.dict.domain.DictionaryDTO;
 
 @Service
 public class DictionaryService {
-	@Value("${naver.client-id}")
-	private String clientId;
+    @Value("${naver.client-id}")
+    private String clientId;
 
-	@Value("${naver.client-secret}")
-	private String clientSecret;
+    @Value("${naver.client-secret}")
+    private String clientSecret;
 
-	@Value("${naver.encyc.url}")
-	private String apiUrl;
+    @Value("${naver.encyc.url}")
+    private String apiUrl;
 
-	private final RestTemplate rt;
+    private final RestTemplate rt;
 
-	public DictionaryService(RestTemplate rt) {
-		this.rt = rt;
-	}
+    public DictionaryService(RestTemplate rt) {
+        this.rt = rt;
+    }
 
-	public DictionaryDTO search(String query, int display, int start) {
-		UriComponents uri = UriComponentsBuilder.fromHttpUrl(apiUrl).queryParam("query", query)
-				.queryParam("display", display).queryParam("start", start).build();
+    public DictionaryDTO search(String query, int display, int start) {
+        UriComponents uri = UriComponentsBuilder
+            .fromHttpUrl(apiUrl)
+            .queryParam("query", query)
+            .queryParam("display", display)
+            .queryParam("start", start)
+            .build();
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("X-Naver-Client-Id", clientId);
-		headers.set("X-Naver-Client-Secret", clientSecret);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", clientId);
+        headers.set("X-Naver-Client-Secret", clientSecret);
 
-		HttpEntity<Void> request = new HttpEntity<>(headers);
-		ResponseEntity<DictionaryDTO> resp = rt.exchange(uri.toUriString(), HttpMethod.GET, request,
-				DictionaryDTO.class);
-		return resp.getBody();
-	}
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<DictionaryDTO> resp = rt.exchange(
+            uri.toUriString(), HttpMethod.GET, request, DictionaryDTO.class);
+        return resp.getBody();
+    }
 }
